3e36e5c00ab15d251ef273891cc5702b3de77797
vestata <tony268596@gmail.com>
2024-02-29 13:38:17 +0800

    Add implementation of `q_sort`
    Implemented `q_sort` in lib/list_sort.c, removing the unused
    (void *priv) parameter for simplification. Introduced a
    compare function `cmp()` that utilizes `strcmp` to compare
    char pointers in each node, facilitating string-based sorting
    in the list.

--- queue.c
+++ queue.c
@@ -4,6 +4,9 @@
 
 #include "queue.h"
 
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
 /* Notice: sometimes, Cppcheck would find the potential NULL pointer bugs,
  * but some of them cannot occur. You can suppress them by adding the
  * following line.
@@ -202,37 +205,157 @@ void q_reverseK(struct list_head *head, int k)
     //     tail = tail->next;
 }
 
-/* Sort elements of queue in ascending/descending order */
+typedef int
+    __attribute__((nonnull(1, 2))) (*list_cmp_func_t)(const struct list_head *,
+                                                      const struct list_head *);
+
+int cmp(const struct list_head *a, const struct list_head *b)
+{
+    element_t *a_ele = container_of(a, element_t, list);
+    element_t *b_ele = container_of(b, element_t, list);
+    return (strcmp(a_ele->value, b_ele->value));
+}
+
+static struct list_head *merge(list_cmp_func_t cmp,
+                               struct list_head *a,
+                               struct list_head *b)
+{
+    struct list_head *head = a, **tail = &head;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (cmp(a, b) <= 0) {
+            *tail = a;
+            tail = &a->next;
+            a = a->next;
+            if (!a) {
+                *tail = b;
+                break;
+            }
+        } else {
+            *tail = b;
+            tail = &b->next;
+            b = b->next;
+            if (!b) {
+                *tail = a;
+                break;
+            }
+        }
+    }
+    return head;
+}
+
+__attribute__((nonnull(1, 2, 3, 4))) static void merge_final(
+    list_cmp_func_t cmp,
+    struct list_head *head,
+    struct list_head *a,
+    struct list_head *b)
+{
+    struct list_head *tail = head;
+    int count = 0;
+
+    for (;;) {
+        /* if equal, take 'a' -- important for sort stability */
+        if (cmp(a, b) <= 0) {
+            tail->next = a;
+            a->prev = tail;
+            tail = a;
+            a = a->next;
+            if (!a)
+                break;
+        } else {
+            tail->next = b;
+            b->prev = tail;
+            tail = b;
+            b = b->next;
+            if (!b) {
+                b = a;
+                break;
+            }
+        }
+    }
+
+    /* Finish linking remainder of list b on to tail */
+    tail->next = b;
+    do {
+        /*
+         * If the merge is highly unbalanced (e.g. the input is
+         * already sorted), this loop may run many iterations.
+         * Continue callbacks to the client even though no
+         * element comparison is needed, so the client's cmp()
+         * routine can invoke cond_resched() periodically.
+         */
+        if (unlikely(!++count))
+            cmp(b, b);
+        b->prev = tail;
+        tail = b;
+        b = b->next;
+    } while (b);
+
+    /* And the final links to make a circular doubly-linked list */
+    tail->next = head;
+    head->prev = tail;
+}
+
+__attribute__((nonnull(1, 2))) void list_sort(struct list_head *head,
+                                              list_cmp_func_t cmp)
+{
+    struct list_head *list = head->next, *pending = NULL;
+    size_t count = 0; /* Count of pending */
+
+    if (list == head->prev) /* Zero or one elements */
+        return;
+
+    /* Convert to a null-terminated singly-linked list. */
+    head->prev->next = NULL;
+
+    do {
+        size_t bits;
+        struct list_head **tail = &pending;
+
+        /* Find the least-significant clear bit in count */
+        for (bits = count; bits & 1; bits >>= 1)
+            tail = &(*tail)->prev;
+        /* Do the indicated merge */
+        if (likely(bits)) {
+            struct list_head *a = *tail, *b = a->prev;
+
+            a = merge(cmp, b, a);
+            /* Install the merged result in place of the inputs */
+            a->prev = b->prev;
+            *tail = a;
+        }
+
+        /* Move one element from input list to pending */
+        list->prev = pending;
+        pending = list;
+        list = list->next;
+        pending->next = NULL;
+        count++;
+    } while (list);
+
+    /* End of input; merge together all the pending lists. */
+    list = pending;
+    pending = pending->prev;
+    for (;;) {
+        struct list_head *next = pending->prev;
+
+        if (!next)
+            break;
+        list = merge(cmp, pending, list);
+        pending = next;
+    }
+    /* The final merge, rebuilding prev links */
+    merge_final(cmp, head, pending, list);
+}
+
 void q_sort(struct list_head *head, bool descend)
 {
     if (!head || list_empty(head) || list_is_singular(head))
         return;
 
-    bool swapped = true;
-    do {
-        swapped = false;
-        struct list_head *current;
-        for (current = head->next; current->next != head;
-             current = current->next) {
-            struct list_head *next = current->next;
-            element_t *currElem = list_entry(current, element_t, list);
-            element_t *nextElem = list_entry(next, element_t, list);
-
-            if (strcmp(currElem->value, nextElem->value) < 0) {
-                // char *temp = currElem->value;
-                // currElem->value = nextElem->value;
-                // nextElem->value = temp;
-                currElem->list.prev->next = next;
-                currElem->list.next = nextElem->list.next;
-                nextElem->list.prev = currElem->list.prev;
-                currElem->list.prev = next;
-                nextElem->list.next->prev = current;
-                nextElem->list.next = current;
-
-                swapped = true;
-            }
-        }
-    } while (swapped);
+    list_sort(head, cmp);
+    return;
 }
 
 /* Remove every node which has a node with a strictly less value anywhere to

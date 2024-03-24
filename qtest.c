2920565e4b682e09bab1461b87775bff8e710300
vestata <tony268596@gmail.com>
2024-03-24 21:25:17 +0800

    Modified ttt command for computer vs computer with MCTS
    Modified the ttt command to set up computer vs computer games
    where both sides use the Monte Carlo Tree Search (MCTS) algorithm.

--- qtest.c
+++ qtest.c
@@ -1052,64 +1052,62 @@ static void print_moves()
     printf("\n");
 }
 
-static int get_input(char player)
-{
-    char *line = NULL;
-    size_t line_length = 0;
-    int parseX = 1;
+// static int get_input(char player)
+// {
+//     char *line = NULL;
+//     size_t line_length = 0;
+//     int parseX = 1;
 
-    int x = -1, y = -1;
-    while (x < 0 || x > (BOARD_SIZE - 1) || y < 0 || y > (BOARD_SIZE - 1)) {
-        printf("%c> ", player);
-        int r = getline(&line, &line_length, stdin);
-        if (r == -1)
-            exit(1);
-        if (r < 2)
-            continue;
-        x = 0;
-        y = 0;
-        parseX = 1;
-        for (int i = 0; i < (r - 1); i++) {
-            if (isalpha(line[i]) && parseX) {
-                x = x * 26 + (tolower(line[i]) - 'a' + 1);
-                if (x > BOARD_SIZE) {
-                    // could be any value in [BOARD_SIZE + 1, INT_MAX]
-                    x = BOARD_SIZE + 1;
-                    printf("Invalid operation: index exceeds board size\n");
-                    break;
-                }
-                continue;
-            }
-            // input does not have leading alphabets
-            if (x == 0) {
-                printf("Invalid operation: No leading alphabet\n");
-                y = 0;
-                break;
-            }
-            parseX = 0;
-            if (isdigit(line[i])) {
-                y = y * 10 + line[i] - '0';
-                if (y > BOARD_SIZE) {
-                    // could be any value in [BOARD_SIZE + 1, INT_MAX]
-                    y = BOARD_SIZE + 1;
-                    printf("Invalid operation: index exceeds board size\n");
-                    break;
-                }
-                continue;
-            }
-            // any other character is invalid
-            // any non-digit char during digit parsing is invalid
-            // TODO: Error message could be better by separating these two cases
-            printf("Invalid operation\n");
-            x = y = 0;
-            break;
-        }
-        x -= 1;
-        y -= 1;
-    }
-    free(line);
-    return GET_INDEX(y, x);
-}
+//     int x = -1, y = -1;
+//     while (x < 0 || x > (BOARD_SIZE - 1) || y < 0 || y > (BOARD_SIZE - 1)) {
+//         printf("%c> ", player);
+//         int r = getline(&line, &line_length, stdin);
+//         if (r == -1)
+//             exit(1);
+//         if (r < 2)
+//             continue;
+//         x = 0;
+//         y = 0;
+//         parseX = 1;
+//         for (int i = 0; i < (r - 1); i++) {
+//             if (isalpha(line[i]) && parseX) {
+//                 x = x * 26 + (tolower(line[i]) - 'a' + 1);
+//                 if (x > BOARD_SIZE) {
+//                     // could be any value in [BOARD_SIZE + 1, INT_MAX]
+//                     x = BOARD_SIZE + 1;
+//                     printf("Invalid operation: index exceeds board size\n");
+//                     break;
+//                 }
+//                 continue;
+//             }
+//             // input does not have leading alphabets
+//             if (x == 0) {
+//                 printf("Invalid operation: No leading alphabet\n");
+//                 y = 0;
+//                 break;
+//             }
+//             parseX = 0;
+//             if (isdigit(line[i])) {
+//                 y = y * 10 + line[i] - '0';
+//                 if (y > BOARD_SIZE) {
+//                     // could be any value in [BOARD_SIZE + 1, INT_MAX]
+//                     y = BOARD_SIZE + 1;
+//                     printf("Invalid operation: index exceeds board size\n");
+//                     break;
+//                 }
+//                 continue;
+//             }
+//             // any other character is invalid
+//             // any non-digit char during digit parsing is invalid
+//             // TODO: Error message could be better by separating these two
+//             cases printf("Invalid operation\n"); x = y = 0; break;
+//         }
+//         x -= 1;
+//         y -= 1;
+//     }
+//     free(line);
+//     return GET_INDEX(y, x);
+// }
 /* ttt code subroutine end*/
 
 static bool do_ttt(int argc, char *argv[])
@@ -1118,7 +1116,6 @@ static bool do_ttt(int argc, char *argv[])
     char table[N_GRIDS];
     memset(table, ' ', N_GRIDS);
     char turn = 'X';
-    char ai = 'O';
 
     while (1) {
         char win = check_win(table);
@@ -1132,30 +1129,18 @@ static bool do_ttt(int argc, char *argv[])
             break;
         }
 
-        if (turn == ai) {
-            int move = mcts(table, ai);
-            if (move != -1) {
-                table[move] = ai;
-                record_move(move);
-            }
-        } else {
-            draw_board(table);
-            int move;
-            while (1) {
-                move = get_input(turn);
-                if (table[move] == ' ') {
-                    break;
-                }
-                printf("Invalid operation: the position has been marked\n");
-            }
+        draw_board(table);
+        int move = mcts(table, turn);
+        if (move != -1) {
             table[move] = turn;
             record_move(move);
         }
         turn = turn == 'X' ? 'O' : 'X';
     }
     print_moves();
+    move_count = 0;
 
-    return 0;
+    return !error_check();
 }
 
 static void console_init()
@@ -1199,7 +1184,10 @@ static void console_init()
     ADD_COMMAND(reverseK, "Reverse the nodes of the queue 'K' at a time",
                 "[K]");
     // ADD_COMMAND(shuffle, "Do Fisher-Yates shuffle", "");
-    ADD_COMMAND(ttt, "tic-tac-toe", "");
+    ADD_COMMAND(ttt,
+                "tic-tac-toe, Switch between Human vs. Computer(hc) or "
+                "Computer vs. Computer(cc) gameplay.",
+                "[mode]");
     add_param("length", &string_length, "Maximum length of displayed string",
               NULL);
     add_param("malloc", &fail_probability, "Malloc failure probability percent",
